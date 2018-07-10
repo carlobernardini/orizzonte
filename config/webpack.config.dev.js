dd52288d0008da36acdc136b77c1815479dfc317
Carlo Bernardini <carlobernardini@gmail.com>
2018-07-10 20:23:53 +0200

    Eslint and webpack config

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,51 @@
+// const path = require('path');
+// const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server'
+    ],
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        hot: true,
+        port: 3000,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [{
+            test: /(\.js|\.jsx)$/,
+            enforce: 'pre',
+            exclude: /node_modules/,
+            use: ['eslint-loader'],
+        }, {
+            test: /(\.js|\.jsx)$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/,
+        }]
+    },
+    resolve: {
+        alias: {
+            'orizzonte': path.resolve(__dirname, '../src')
+        },
+        modules: [path.resolve(__dirname, '../node_modules')],
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('dev'),
+            },
+        }),
+    ]
+};
