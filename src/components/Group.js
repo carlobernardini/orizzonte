7fe60a745985c26ef3759b9046e4909341b990fd
Carlo Bernardini <bernardini@textkernel.nl>
2018-07-12 17:11:40 +0200

    Improved group removal button icon

--- src/components/Group.js
+++ src/components/Group.js
@@ -55,7 +55,7 @@ class Group extends Component {
                 className="orizzonte__group-btn"
                 onClick={ this.removeGroup }
             >
-                &times;
+                &nbsp;
             </button>
         );
     }
