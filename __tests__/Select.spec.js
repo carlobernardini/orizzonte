7b7a367c45114660700107d8e9356ec94ac8f32b
Carlo Bernardini <carlobernardini@gmail.com>
2018-07-14 22:26:19 +0200

    Added tests for Select component

---
+++ __tests__/Select.spec.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Select from '../src/components/Select';
+
+describe('<Select />', () => {
+    it('should render a normal select', () => {
+        const wrapper = shallow(
+            <Select
+                label="Test select"
+                options={ [{
+                    label: 'Test value 1',
+                    value: 1
+                }, {
+                    label: 'Test value 2',
+                    value: 2
+                }, {
+                    label: 'Test value 3',
+                    value: 3
+                }]}
+                onUpdate={() => {}}
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a disabled select', () => {
+        const wrapper = shallow(
+            <Select
+                label="Test select"
+                options={ [{
+                    label: 'Test value 1',
+                    value: 1
+                }, {
+                    label: 'Test value 2',
+                    value: 2
+                }, {
+                    label: 'Test value 3',
+                    value: 3
+                }]}
+                disabled
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a select with selected value', () => {
+        const wrapper = shallow(
+            <Select
+                label="Test select"
+                options={ [{
+                    label: 'Test value 1',
+                    value: 1
+                }, {
+                    label: 'Test value 2',
+                    value: 2
+                }, {
+                    label: 'Test value 3',
+                    value: 3
+                }]}
+                value={ 3 }
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
