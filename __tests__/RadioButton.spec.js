9d633f003faa1e98e32bb5d5c011098434f2b231
Carlo Bernardini <carlobernardini@gmail.com>
2018-07-16 20:57:17 +0200

    Updated tests for RadioButton component

--- __tests__/RadioButton.spec.js
+++ __tests__/RadioButton.spec.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import RadioButton from '../src/components/RadioButton';
 
+const onChange = jest.fn();
+
 describe('<RadioButton />', () => {
     it('should render a radio button', () => {
+        const value = 'test';
         const wrapper = shallow(
             <RadioButton
                 id="testRadio1"
                 name="testRadioGroup"
                 label="Test radio"
-                value="test"
-                onChange={ () => {}}
+                value={ value }
+                onChange={ onChange }
             />
         );
 
+        wrapper.find('.orizzonte__radio-input').simulate('change');
+        expect(onChange).toHaveBeenCalledWith(value);
         expect(wrapper).toMatchSnapshot();
     });
 
