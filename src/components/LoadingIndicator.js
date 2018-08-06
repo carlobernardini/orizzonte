3d20de1d4eb2dfb62fa83fa516cd790e90721c9e
Carlo Bernardini <bernardini@textkernel.nl>
2018-08-06 15:17:25 +0200

    Fixed tests for LoadingIndicator component

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -6,10 +6,15 @@ const LoadingIndicator = ({ size }) => (
     <svg
         className="orizzonte__loading"
         viewBox={ [0, 0, 44, 44] }
-        style={{
-            width: `${ size }px`,
-            height: `${ size }px`
-        }}
+        style={ ((s) => {
+            if (!s) {
+                return null;
+            }
+            return {
+                width: `${ s }px`,
+                height: `${ s }px`
+            };
+        })(size) }
     >
         <circle
             className="orizzonte__loading-path"
