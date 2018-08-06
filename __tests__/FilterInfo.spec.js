986e399cb15dd3eb75debd846c7d377062f2a8bc
Carlo Bernardini <bernardini@textkernel.nl>
2018-08-06 15:26:37 +0200

    Added tests for FilterInfo component

---
+++ __tests__/FilterInfo.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import FilterInfo from '../src/components/FilterInfo';
+
+describe('<FilterInfo />', () => {
+    it('should render a default filter info tooltip', () => {
+        const wrapper = shallow(
+            <FilterInfo
+                information="Some filter information"
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should not render anything if prop is empty', () => {
+        const wrapper = shallow(
+            <FilterInfo />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
